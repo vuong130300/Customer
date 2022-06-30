216d3625887e9b6cf585fda05fc92537903916e5
vuongpham <today130300@gmail.com>
2022-06-30 18:00:31 +0700

    commit carosel

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-
+import {Carousel} from "react-bootstrap"
 import Button from './Button'
-
+const imga = "https://cdn.tgdd.vn/bachhoaxanh/banners/2505/thuc-pham-dong-lanh-08062022133258.jpg" ;
+const imgb = "https://cdn.tgdd.vn/bachhoaxanh/banners/2505/sua-gia-dai-ly-1506202210212.jpg";
+const imgc = "https://cdn.tgdd.vn/bachhoaxanh/banners/2505/nuoc-yen-17062022132744.png";
 const HeroSlider = props => {
 
     const data = props.data
@@ -37,30 +39,29 @@ const HeroSlider = props => {
     }, [nextSlide, timeOut, props])
 
     return (
-        <div className="hero-slider">
-            {
-                data.map((item, index) => (
-                    <HeroSliderItem key={index} item={item} active={index === activeSlide}/>
-                ))
-            }
-            {
-                props.control ? (
-                    <div className="hero-slider__control">
-                        <div className="hero-slider__control__item" onClick={prevSlide}>
-                            <i className="bx bx-chevron-left"></i>
-                        </div>
-                        <div className="hero-slider__control__item">
-                            <div className="index">
-                                {activeSlide + 1}/{data.length}
-                            </div>
-                        </div>
-                        <div className="hero-slider__control__item" onClick={nextSlide}>
-                            <i className="bx bx-chevron-right"></i>
-                        </div>
-                    </div>
-                ) : null
-            }
-        </div>
+        <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={imga}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={imgb}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={imgc}
+            alt="Third slide"
+          />  
+        </Carousel.Item>
+      </Carousel>
     )
 }
 
@@ -71,33 +72,33 @@ HeroSlider.propTypes = {
     timeOut: PropTypes.number
 }
 
-const HeroSliderItem = props => (
+// const HeroSliderItem = props => (
    
-    <div className={`hero-slider__item ${props.active ? 'active' : ''}`}>
-        <div className="hero-slider__item__info">
-            <div className={`hero-slider__item__info__title color-${props.item.color}`}>
-                <span>{props.item.title}</span>
-            </div>
-            <div className="hero-slider__item__info__description">
-                <span>{props.item.description}</span>
-            </div>
-            <div className="hero-slider__item__info__btn">
-                <Link to={props.item.path}>
-                    <Button
-                        backgroundColor={props.item.color}
-                        icon="bx bx-cart"
-                        animate={true}
-                    >
-                        xem chi tiết
-                    </Button>
-                </Link>
-            </div>
-        </div>
-        <div className="hero-slider__item__image">
-            <div className={`shape bg-${props.item.color}`}></div>
-            <img src={props.item.img} alt="" />
-        </div>
-    </div>
-)
+//     <div className={`hero-slider__item ${props.active ? 'active' : ''}`}>
+//         <div className="hero-slider__item__info">
+//             <div className={`hero-slider__item__info__title color-${props.item.color}`}>
+//                 <span>{props.item.title}</span>
+//             </div>
+//             <div className="hero-slider__item__info__description">
+//                 <span>{props.item.description}</span>
+//             </div>
+//             <div className="hero-slider__item__info__btn">
+//                 <Link to={props.item.path}>
+//                     <Button
+//                         backgroundColor={props.item.color}
+//                         icon="bx bx-cart"
+//                         animate={true}
+//                     >
+//                         xem chi tiết
+//                     </Button>
+//                 </Link>
+//             </div>
+//         </div>
+//         <div className="hero-slider__item__image">
+//             <div className={`shape bg-${props.item.color}`}></div>
+//             <img src={props.item.img} alt="" />
+//         </div>
+//     </div>
+// )
 
 export default HeroSlider
